817d0644070db14ab3967738f4b0462a8893b8ed
Edward <glezmoradev@gmail.com>
2026-01-22 05:08:31 -0500

    minorChanges

--- Jwt_Project/obj/Debug/net10.0/Jwt_Project.AssemblyInfo.cs
+++ Jwt_Project/obj/Debug/net10.0/Jwt_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jwt_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b845c4b482d4cbfe6fa9670389b9c941c72b10b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jwt_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jwt_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
